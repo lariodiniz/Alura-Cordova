4dc6e7ef6748dd647d8bbdcdf7528ae520095536
LarioDiniz <lario.diniz@palo.com.br>
2018-02-19 16:31:05 -0300

    Aula 07
    Aula 07

--- garconapp/platforms/browser/www/js/app.js
+++ garconapp/platforms/browser/www/js/app.js
@@ -1,16 +1,19 @@
 $('.collection')
     .on('click', '.collection-item', function(){
+        var nomeProduto = this.firstChild.textContent;
+        Materialize.toast(nomeProduto + ' adicionado', 1000);
 
         var $badge = $('.badge', this);
         if ($badge.length === 0) {
-            $badge = $('<span class="badge brown-text">0</span>')
-                        .appendTo(this);
+            $badge = $('<span class="badge brown-text">0</span>').appendTo(this);
         }
 
         $badge.text(parseInt($badge.text()) + 1);
-		var nomeProduto = this.firstChild.textContent;
-		Materialize.toast(nomeProduto + ' adicionado', 1000);
     })
+    .on('click', '.badge', function() {
+        $(this).remove();
+        return false;
+    });
 
 $('.modal-trigger').leanModal();
 
@@ -25,12 +28,6 @@ $('#confirmar').on('click', function() {
     $('#resumo').empty().text(texto);
 });
 
-$('.collection')
-    .on('click', '.badge', function() {
-        $(this).remove();
-        return false;
-    });
-	
 $('.acao-limpar').on('click', function() {
     $('#numero-mesa').val('');
     $('.badge').remove();
